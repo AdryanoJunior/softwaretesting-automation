bff0d8d646a3da8646eb596d689cd29a3a6eeac1
Adryano Junior <jusamsung10@gmail.com>
2025-07-16 13:33:02 -0300

    changes to cypress commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,13 @@
 Cypress.Commands.add('login', (email, password) => {
-    cy.visit('/customer/account')
+    cy.visit('/customer/account/login')
     cy.get("[title='Email']").type('cristianoronaldo@gmail.com')
     cy.get("[title='Password']").type('test12345$')
+    cy.get("[name='send']").eq(0).click()
+})
+
+Cypress.Commands.add('loginFail', (email, password) => {
+    cy.visit('/customer/account/login')
+    cy.get("[title='Email']").type('wrongcredentials@gmail.com')
+    cy.get("[title='Password']").type('test123$')
     cy.get("[name='send']").eq(0).click()
 })
